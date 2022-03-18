a9207fbb15703a047bae8fd0bf6b56ce43c9322c
Rocky.C <cloki0610@gmail.com>
2022-03-18 01:46:37 +0000

    Get json datat from api and asset json file

--- angular-scrabble/src/app/app.component.ts
+++ angular-scrabble/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
+import players from '../assets/players.json';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +10,12 @@ import { DomSanitizer } from '@angular/platform-browser';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  public playerList: { PlayerId: number; Name: string }[] = players.Players;
+  public result: Array<any> = new Array<any>();
   constructor(
     private matIconRegistry: MatIconRegistry,
-    private domSanitizer: DomSanitizer
+    private domSanitizer: DomSanitizer,
+    private http: HttpClient
   ) {
     this.matIconRegistry.addSvgIcon(
       'callus',
@@ -24,5 +29,17 @@ export class AppComponent {
       'trophy',
       this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/trophy.svg')
     );
+  }
+  ngOnInit() {
+    this.getData();
+  }
+  getData() {
+    this.http
+      .get<any>('https://mocki.io/v1/dac50ece-1793-4e95-9040-fbb6cd2dbb7a')
+      .subscribe((response: any) => {
+        this.result = response.Results;
+        console.log(this.result);
+        console.log(this.playerList)
+      });
   }
 }
